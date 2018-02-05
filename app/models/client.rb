237db7aa7a0c97d4ee8d8cd29c62702b4d599aa4
Yujun Sung <yujunsung@gmail.com>
2018-02-05 16:35:05 +0000

    add item_contrller.rb

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,4 +1,14 @@
 class Client < ActiveRecord::Base
   belongs_to :user
-  has_many :items
+  has_many :orders
+
+
+  def slug
+    username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect {|s| slug == s.slug}
+  end
+
 end
