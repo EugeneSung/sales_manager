cae50b8bed13faf0ba587319f7329f1ee8b9922e
Yujun Sung <yujunsung@gmail.com>
2018-02-07 04:43:10 +0000

    add add_order.erb and control

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -18,13 +18,22 @@ class ClientController < ApplicationController
   get '/clients/:slug' do
     @client = Client.find_by_slug(params[:slug])
     if logged_in?
-      binding.pry
+      #binding.pry
       erb :"clients/show_client"
     else
       redirect '/login'
     end
   end
-  get '/add_order' do
+  get '/:slug/add_order' do
+    @client = Client.find_by_slug(params[:slug])
+    erb :"orders/add_order"
+  end
+  post '/:slug/add_order' do
+
+    @client = Client.find_by_slug(params[:slug])
+    @order = Order.create(params[:order])
+    @order.client_id = @client.id
+    @order.save
 
   end
 
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,4 @@
-class Order < ActiveRecord::Base
+class OrderItem < ActiveRecord::Base
 
  belongs_to :item
  belongs_to :order
