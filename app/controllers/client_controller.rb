6d0e7df39484d6b513c32d460395d0a8af00aeea
Yujun Sung <yujunsung@gmail.com>
2018-02-21 03:11:39 +0000

    validate if client already exist

--- app/controllers/client_controller.rb
+++ app/controllers/client_controller.rb
@@ -6,13 +6,19 @@ class ClientController < ApplicationController
     erb :'/clients/add_client'
   end
   post "/clients/add_client" do
-    if logged_in?
-      @client = Client.create(params)
-      @client.user_id = current_user.id
-      @client.save
-      redirect '/'
+
+    client = Client.find_by(name: params[:name])
+    if client
+      redirect '/clients/add_client'
     else
-      redirect '/login'
+      if logged_in?
+        @client = Client.create(params)
+        @client.user_id = current_user.id
+        @client.save
+       redirect '/'
+      else
+        redirect '/login'
+      end
     end
   end
   get '/clients/:slug' do
@@ -25,12 +31,13 @@ class ClientController < ApplicationController
     end
   end
   get '/clients/:slug/edit' do
+
     @client = Client.find_by_slug(params[:slug])
-    if logged_in?
-      #binding.pry
-      erb :"/clients/edit_client"
+    if @client.user_id == current_user.id
+          #binding.pry
+        erb :"/clients/edit_client"
     else
-      redirect '/login'
+      redirect "/"
     end
   end
 
